18ac4c67c84ec0a669dc41e5df0263cb58b9c47a
Fernando Okuma <fernando.okuma@lambda3.com.br>
2020-06-18 02:03:38 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# LedMatrixRestAPI
+# Led Matrix RestAPI
+Projeto arduino que conectar a redes wi-fi e carrega um API Rest para receber comandos para acionar uma matrix de leds RGB
+
+## Ferramentas e bibliotecas
+- Arduino IDE ([arduino.cc](https://www.arduino.cc))
+- Bibliotecas da placa Wemos D1 ([Tutorial para instalação](https://blogmasterwalkershop.com.br/embarcados/wemos/wemos-d1-configurando-a-ide-do-arduino/))
+- Biblioteca para controle de leds RGB FastLED ([fastled.io](https://fastled.io))
+- Postman ([postman.com](https://www.postman.com/))
+
+## Hardware
+- Placa Wemos D1 ([Wemos D1](https://www.filipeflop.com/produto/placa-wemos-d1-r2-wifi-esp8266/)) ou ([Wemos D1 Mini](https://www.filipeflop.com/produto/placa-wemos-d1-mini-pro-wifi-esp8266/))
+- Matriz de Leds RGB WS2812b ([WS2812b](https://www.filipeflop.com/produto/painel-de-led-rgb-x16-ws2812-5050/?gclid=CjwKCAjw_qb3BRAVEiwAvwq6VlFsxFiCEGXGkRQ5Az3WoAN2fasJSACHhEla_VCaw_X2dU4IQ4iVERoC_8sQAvD_BwE))
+- Conversor de Nível Lógico 3,3V para 5V ([Conversor Step Up](https://www.filipeflop.com/produto/conversor-de-nivel-logico-33-5v-bidirecional/))
+
+## Montagem
+
+## Executando a aplicação Rest API
+Ao gravar o firmware na Wemos D1 será possível receber os logs através do monitor serial do Arduino IDE, onde serão será possível verificar os logs do processo de conexão com a rede wireless configurada hardcoded. Para isso, será necessário manter a placa conectada à porta usb, utilizada para fazer a gravação do firmware. Ao final da inicialização, se as configurações de rede estiverem corretas, a saída no monitor serial do Arduino IDE será semelhando a seguinte
+
+```
+Connecting to WiFi AP..........
+Connected to YourNetworkSSID --- IP:192.168.1.200
+HTTP REST Server Started
+```
+
+Com esta informação já sabemos onde podemos enviar os comandos para controlar as cores dos leds WS2812b. 
+
+## Consumindo a API
+Nossa API tem apenas dois endpoints. O raiz retorna simplesmente um texto de saudação da API que é para mostrar o quando o nosso firmware é simpático.
+
+**Enviando Post para alterar cor dos leds**
+
+O json enviado, no método POST, para a Wemos D1 tem duas informações, **numLeds** e **status** indicando a quantidade de leds WS2812b que serão alterardos e a cor respectivamente. Os status possível são:
+
+- 1: Verde;
+- 2: Vermelho;
+- 3: Amarelo;
+
+Para o nosso exemplo, o nosso endpoint será: **http://192.168.1.200/leds**
+
+```
+POST /leds HTTP/1.1
+Host: 192.168.1.200
+Connection: close
+Content-Type: application/json
+Content-Length: 35
+
+{
+	"numLeds": 16,
+	"status": 1
+}
+```
+
+O método GET retorná o conteúdo do último POST recebido
+
+```
+HTTP/1.1 200 Ok
+Content-Type: application/json
+Content-Length: 30
+Connection: close
+
+{"numLeds":16,"status":"1"}
+```
---
+++ WeMosServer.ino
@@ -0,0 +1,174 @@
+//#define FASTLED_ESP8266_RAW_PIN_ORDER
+//#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+//#define FASTLED_ESP8266_D1_PIN_ORDER
+#include <FastLED.h>
+#include <stdio.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+
+#define NUM_LEDS 12
+#define DATA_PIN 13
+
+CRGB leds[NUM_LEDS];
+
+#define HTTP_REST_PORT 80
+#define WIFI_RETRY_DELAY 500
+#define MAX_WIFI_INIT_RETRY 50
+
+struct Led
+{
+    byte numLeds;
+    byte status;
+} led_resource;
+
+const char *wifi_ssid = "[WIFI_SSID]";
+const char *wifi_passwd = "[WIFI_PASSWD]";
+
+ESP8266WebServer http_rest_server(HTTP_REST_PORT);
+
+void init_led_resource()
+{
+    led_resource.numLeds = NUM_LEDS;
+    led_resource.status = LOW;
+}
+
+int init_wifi()
+{
+    int retries = 0;
+
+    Serial.println("Connecting to WiFi AP..........");
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(wifi_ssid, wifi_passwd);
+
+    while ((WiFi.status() != WL_CONNECTED) && (retries < MAX_WIFI_INIT_RETRY))
+    {
+        retries++;
+        delay(WIFI_RETRY_DELAY);
+        Serial.print("#");
+    }
+    return WiFi.status();
+}
+
+void get_leds()
+{
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject &jsonObj = jsonBuffer.createObject();
+    char JSONmessageBuffer[200];
+
+    if (led_resource.numLeds == 0)
+        http_rest_server.send(204);
+    else
+    {
+        jsonObj["numLeds"] = led_resource.numLeds;
+        jsonObj["status"] = led_resource.status;
+        jsonObj.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+        http_rest_server.send(200, "application/json", JSONmessageBuffer);
+    }
+}
+
+void json_to_resource(JsonObject &jsonBody)
+{
+    int numLeds, status;
+
+    numLeds = jsonBody["numLeds"];
+    status = jsonBody["status"];
+
+    Serial.println(numLeds);
+    Serial.println(status);
+
+    led_resource.numLeds = numLeds;
+    led_resource.status = status;
+}
+
+void post_leds()
+{
+    StaticJsonBuffer<500> jsonBuffer;
+    String post_body = http_rest_server.arg("plain");
+    Serial.println(post_body);
+
+    JsonObject &jsonBody = jsonBuffer.parseObject(http_rest_server.arg("plain"));
+
+    Serial.print("HTTP Method: ");
+    Serial.println(http_rest_server.method());
+
+    if (!jsonBody.success())
+    {
+        Serial.println("error in parsin json body");
+        http_rest_server.send(400);
+    }
+    else
+    {
+        json_to_resource(jsonBody);
+        http_rest_server.sendHeader("Location", "/leds/" + String(led_resource.status));
+        http_rest_server.send(201);
+        setFastLED(led_resource.status);
+    }
+}
+
+void setFastLED(byte led)
+{
+    CRGB color;
+
+    switch (led)
+    {
+        case 1:
+            color = CRGB::Green;
+            break;
+        case 2:
+            color = CRGB::Red;
+            break;
+        case 3:
+            color = CRGB::Orange;
+            break;
+        default:
+            color = CRGB::Black;
+            break;
+    }
+
+    for(char i = 0; i < led_resource.numLeds; i++)
+        leds[i] = color;
+
+    FastLED.show();
+}
+
+void config_rest_server_routing()
+{
+    http_rest_server.on("/", HTTP_GET, []() {
+        http_rest_server.send(200, "text/html",
+                              "Welcome to the ESP8266 REST Web Server");
+    });
+    http_rest_server.on("/leds", HTTP_GET, get_leds);
+    http_rest_server.on("/leds", HTTP_POST, post_leds);
+}
+
+void setup(void)
+{
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+    Serial.begin(115200);
+
+    init_led_resource();
+    if (init_wifi() == WL_CONNECTED)
+    {
+        Serial.print("Connected to ");
+        Serial.print(wifi_ssid);
+        Serial.print(" --- IP: ");
+        Serial.println(WiFi.localIP());
+    }
+    else
+    {
+        Serial.print("Error connecting to: ");
+        Serial.println(wifi_ssid);
+    }
+
+    config_rest_server_routing();
+
+    http_rest_server.begin();
+    Serial.println("HTTP REST Server Started");
+}
+
+void loop(void)
+{
+    http_rest_server.handleClient();
+}
